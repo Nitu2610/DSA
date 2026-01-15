60d57c32caf194dea8eb69eed375370fe80c6108
Nitu2610 <nikss26796@gmail.com>
2026-01-15 14:25:51 +0530

    completed the insertion sorting.

---
+++ Probelms/4- Sorting/Insertion.js
@@ -0,0 +1,35 @@
+// INsertion - Take the element and place it in the correct index in ascending order.
+
+// Time Complexity -> Best Case (Sorted) is O(N),  Average Case (Random) and Worst Case (Reverse) is O(N^2).
+// Space Complexity -> for the 3 cases is O(1).
+
+// let arr = [1, 2, 3]; // [6, 2, 3, 5, 4, 1];
+// // console.log(`Before- `, arr);
+// for (let i = 1; i < arr.length; i++) {
+//   let didSwap = false;
+//   for (let j = i; j > 0; j--) {
+//     if (arr[j] < arr[j - 1]) {
+//       let temp = arr[j];
+//       arr[j] = arr[j - 1];
+//       arr[j - 1] = temp;
+//       console.log(arr);
+//     }
+//   }
+
+//   // let j=i;
+//   // while (j>0 && arr[j]<arr[j-1]){
+//   //     let temp= arr[j];
+//   //     arr[j]=arr[j-1];
+//   //     arr[j-1]=temp;
+//   //     j--;
+//   //     console.log(arr)
+//   // }
+
+//   if (!didSwap) {
+//     console.log(`The given array- ${arr} is already sorted.!!`);
+//     break;
+//   }
+
+//   console.log("---------");
+// }
+
