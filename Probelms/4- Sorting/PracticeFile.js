2da04c41cb856b2a2b67584ae184c9b9dcca02c1
Nitu2610 <nikss26796@gmail.com>
2026-01-24 23:23:48 +0530

    practiced the merge sort alog. once.

---
+++ Probelms/4- Sorting/PracticeFile.js
@@ -0,0 +1,39 @@
+let arr = [5, 9, 7, 4, 1, 2, 3, 0, 8];
+
+const merge = (arr, low, mid, high) => {
+  let temp = [],left = low, right = mid + 1;
+
+  while (left <= mid && right <= high) {
+    if (arr[left] <= arr[right]) {
+      temp.push(arr[left]);
+      left++;
+    } else {
+      temp.push(arr[right]);
+      right++;
+    }
+  }
+  while (left <= mid) {
+    temp.push(arr[left]);
+    left++;
+  }
+  while (right <= high) {
+    temp.push(arr[right]);
+    right++;
+  }
+  for (let i = 0; i < temp.length; i++) {
+    arr[low + i] = temp[i];
+  }
+};
+
+const mergeSort = (arr, low = 0, high = arr.length - 1) => {
+  if (low >= high) return;
+
+  let mid = Math.floor((low + high) / 2);
+  mergeSort(arr, low, mid);
+  mergeSort(arr, mid + 1, high);
+  merge(arr, low, mid, high);
+};
+
+mergeSort(arr);
+
+console.log(arr)
