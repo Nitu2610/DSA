ccdd57a62c49d825576ad2e7e1657689610c1dc3
Nitu2610 <nikss26796@gmail.com>
2026-02-14 00:57:42 +0530

    no major update.

--- Re-Practice Concept- Problems.js
+++ Re-Practice Concept- Problems.js
@@ -1,30 +1,31 @@
 let arr = [1, 2, 0, 6, 5, 8, 9, 7, 3, 4];
 
-const quick=(arr, low=0,high)=>{
-  let pivot=arr[low],i=low, j=high;
+function pivotedIndex(arr,low,high){
+let pivot=arr[low], i= low, j= high;
 
-  while(i<j){
-    while(arr[i]<=pivot && i<= high-1){
-      i++;
-    }
-       while(arr[j]>pivot && i>= low+1){
-      j--;
-    }
-    if(i<j){
-      [arr[i],arr[j]]=  [arr[j],arr[i]]
-    }
+while(i<j){
+
+  while(arr[i]<=pivot && i<= high-1){
+    i++;
   }
-   [arr[low],arr[j]]=  [arr[j],arr[low]];
-   return j;
+   while(arr[j]> pivot && j>= low+1){
+    j--;
+  }
+  if(i<j){
+    [ arr[i], arr[j] ] =  [ arr[j], arr[i] ]
+  }
+}
+ [ arr[low], arr[j] ] =  [ arr[j], arr[low] ];
+ return j;
 }
 
-const quickSort=(arr, low=0,high=arr.length-1)=>{
+function quickSort(arr,low=0,high=arr.length-1){
   if(low<high){
-    const piovetIndex=quick(arr, low,high);
-    quickSort(arr, low, piovetIndex-1);
-    quickSort(arr, piovetIndex+1,high)
+    let p=pivotedIndex(arr,low,high);
+    quickSort(arr,low, p-1);
+    quickSort(arr,p+1,high)
   }
 }
 
-quickSort(arr);
+quickSort(arr)
 console.log(arr)
