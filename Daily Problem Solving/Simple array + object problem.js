cb9805b0ce5dbf914872e9bf6a056354e0f78cb0
Nitu2610 <nikss26796@gmail.com>
2026-05-30 18:46:27 +0530

    updated the problems practice today.

--- Daily Problem Solving/Simple array + object problem.js
+++ Daily Problem Solving/Simple array + object problem.js
@@ -16,11 +16,62 @@ function groupUsersByAge(users) {
 			res[ele.age].push(ele.name)
 		}
 		else {
-			let val=[];
-			val.push(ele.name)
-			res[ele.age]= val;
+			res[ele.age]= [ele.name];
 		}
 	}
 	console.log(res)
 }
 groupUsersByAge(users)
+
+
+//--------------------------------------------
+
+const names = ["Nitu", "Rahul", "Nitu", "Asha", "Rahul"];
+const seen= new Set(names);
+console.log(seen.size)
+// ----------------------------
+const entries = ["Nitu", "Rahul", "Asha", "Rahul"];
+
+const seen= new Set();
+
+const checkDup=()=> {
+    for(let ele of entries){
+        if (seen.has(ele) ) {
+            console.log(`${ele} already entered`);
+        } else {
+            seen.add(ele)
+        }
+        }
+    }
+checkDup();
+// ----------------------------------
+const oldStudents = ["Nitu", "Rahul", "Asha"];
+
+const newRegistrations = [
+  "Rahul",
+  "Asha",
+  "Vikram",
+  "Priya"
+];
+
+const oldSet= new Set(oldStudents);
+let res=[];
+for (let ele of newRegistrations){
+    if( ! oldSet.has(ele)){
+        res.push(ele)
+    }
+}
+console.log(res)
+// ------------------------
+const nums = [2, 5, 1, 2, 3, 5, 1];
+
+const seen=new Set();
+
+for (let num of nums){
+    if(!seen.has(num)){
+        seen.add(num)
+    }
+    else {
+        console.log(num); break;
+    }
+}
