f7f198b2a0a867d0b65abe01df1766b85ef3fb33
Nitu2610 <nikss26796@gmail.com>
2026-08-14 09:50:38 +0530

    folder restructure.

---
+++ daily_practice/strings.js
@@ -0,0 +1,36 @@
+// String Problem 
+// ------------------------------------------------
+// #1: Count Vowels
+// let str = "programming";
+
+// let count = 0;
+// let vowel = "aeiou";
+// for (let i = 0; i < str.length; i++) {
+//   if (vowel.includes(str[i])) {
+//     count++;
+//   }
+// }
+
+// console.log(count);
+// ------------------------------------------------
+// #2: Palindrome Check
+// let str = "abccba";
+
+// let left = 0;
+// right = str.length - 1;
+// let flag = true;
+// while (left < right) {
+//   if (str[left] !== str[right]) {
+//     flag = false;
+//     break;
+//   }
+// }
+// left++;
+// right--;
+
+// console.log(flag);
+// ------------------------------------------------
+
+
+
+
